3d8c77aa5c8a03ad343a7f6021efae5591533dd5
abssujan <abdussabursujan@gmail.com>
2024-04-01 01:06:46 +0600

    set nuber validation

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ let lists = JSON.parse(localStorage.getItem('list') || '[]')
 function showList(){
     showListBox.innerHTML = '';
 
-    lists.forEach(list =>{
+    lists.forEach((list, index) =>{
         let listDiv = `
         <div class="values">
         <!-- date -->
@@ -20,7 +20,7 @@ function showList(){
         <div class="value"><span>${list.number}</span></div>
         <!-- Ammount -->
         <div class="value"><span>${list.ammount}</span></div>
-        <div class="icons">
+        <div onclick="deleteList(${index})" class="icons">
         <i class="fa-solid fa-delete-left"></i>
         </div>
         </div>
@@ -31,10 +31,18 @@ function showList(){
 }
 showList()
 
+function deleteList(listIndex){
+    lists.splice(listIndex, 1); // removing from arry in localstorage
+    // update local storage
+    localStorage.setItem('list', JSON.stringify(lists));
+    showList()
+
+}
+
 submitBtn.addEventListener('click', function(){
     const numberInputValue = numberInputField.value;
     const ammountInputValue = ammountInputField.value;
-    if(numberInputValue || ammountInputValue){
+    if(!isNaN(numberInputValue) && numberInputValue !== ''){
         let dateObject = new Date();
         let month = months[dateObject.getMonth()];
         let day = dateObject.getDate();
@@ -47,8 +55,11 @@ submitBtn.addEventListener('click', function(){
         lists.push(listInformation);
         // saving local storage
         localStorage.setItem("list", JSON.stringify(lists));
-        numberInputField.value = '';
-        ammountInputField.value = '';
+        
+    }else {
+        alert('Please enter a valid number')
     }
+    numberInputField.value = '';
+    ammountInputField.value = '';
     showList()
 })
